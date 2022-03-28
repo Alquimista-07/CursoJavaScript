7d9f7503847804060b5c3e9d0da93b4afa82d297
Alquimista-07 <jdca.07@gmail.com>
2022-03-28 14:25:30 -0500

    5. Comprendiendo la consola

---
+++ Sección_1_Empezar_Aquí/2. JavaScript/js/app.js
@@ -0,0 +1,9 @@
+var a = 1;
+var b = "Juan";
+
+console.log( a );
+console.warn( a ); // Warning
+console.error( a ); //Error sin detener el código
+console.info( a ); // Información
+
+console.log( a + a ); // Puedo hacer operaciones
