31fb536f2781979d55f83f4433be9a150477fc6e
Alquimista-07 <jdca.07@gmail.com>
2022-04-05 11:38:59 -0500

    38. Palabras reservadas de JavaScript

---
+++ Sección_4_Interpretacion, palabras reservadas, decisiones, escape y cookies/3. JavaScript/js/app.js
@@ -0,0 +1,133 @@
+/*
+	Palabras reservadas
+	Y buenas practicas
+ */
+
+// Estas palabras como su nombre indica son reservadas del lenguaje y no podemos usarlas para definir por ejemplo una variable
+// ya que nos va a dar error de sintáxis
+
+// abstract
+
+// boolean
+
+// break
+
+// byte
+
+// case
+
+// catch
+
+// char
+
+// class
+
+// const
+
+// continue
+
+// debugger
+
+// default
+
+// delete
+
+// do
+
+// double
+
+// else
+
+// enum
+
+// export
+
+// extends
+
+// false
+
+// final
+
+// finally
+
+// float
+
+// for
+
+// function
+
+// goto
+
+// if
+
+// implements
+
+// import
+
+// in
+
+// instanceof
+
+// int
+
+// interface
+
+// long
+
+// native
+
+// new
+
+// null
+
+// package
+
+// private
+
+// protected
+
+// public
+
+// return
+
+// short
+
+// static
+
+// super
+
+// switch
+
+// synchronized
+
+// this
+
+// throw
+
+// throws
+
+// transient
+
+// true
+
+// try
+
+// typeof
+
+// var
+
+// void
+
+// volatile
+
+// while
+
+// with
+
+
+
+
+
+
+
+
